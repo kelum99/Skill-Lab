55832f478b811cffcdfbb540051d12f8f0c74694
Erandi <roshanie101@gmail.com>
2021-09-08 21:18:44 +0530

    student

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Test from "../src/pages/Finance Management/Test";
+import Test from "./pages/Finance Management/Test";
+import AddMarks from "./pages/Student Management/AddMarks";
+import Enroll from "./pages/Student Management/Enroll";
+import MyCourses from "./pages/Student Management/MyCourses";
+import MyPerformance from "./pages/Student Management/MyPerformance";
+import UpdateEnroll from "./pages/Student Management/UpdateEnroll";
+import UpdateMarks from "./pages/Student Management/UpdateMarks";
+import ViewMarks from "./pages/Student Management/ViewMarks";
+
 
 function MainRouter() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/test" exact component={Test} />
-      </Switch>
-    </Router>
+    <>
+       <Route path="/test" exact component={Test}/>
+        <Route path="/enroll" exact component={Enroll}/>
+        <Route path="/MyCourses" exact component={MyCourses}/>
+        <Route path="/MyPerformance" exact component={MyPerformance}/>
+        <Route path="/UpdateEnroll" exact component={UpdateEnroll}/>
+        <Route path="/AddMarks" exact component={AddMarks} />
+        <Route path="/UpdateMarks" exact component={UpdateMarks} />
+        <Route path="/ViewMarks" exact component={ViewMarks} />
+      </>
   );
 }
 
---
+++ src/pages/Student Management/MyPerformance.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Table, Input } from 'antd';
+
+
+function MyPerformance(){
+
+        //table
+        const columns = [
+            {
+                title: '#',
+                dataIndex: 'index',
+            },
+            {
+                title: 'Subject',
+                dataIndex: 'subject',
+            },
+            {
+                title: 'Course',
+                dataIndex: 'course',
+            },
+            {
+                title: 'Module',
+                dataIndex: 'module',
+            },
+            {
+                title: 'Assignment Code',
+                dataIndex: 'code',
+            },
+            {
+                title: 'Result',
+                dataIndex: 'result',
+            },
+        ];
+        const data = [
+            {
+                key: '1',
+                index: '1',
+                subject: 'Subject',
+                course: 'Course',
+                module: 'Module',
+                code: 'Code',
+                result: 'Result'
+            },
+            {
+                key: '2',
+                index: '1',
+                subject: 'Subject',
+                course: 'Course',
+                module: 'Module',
+                code: 'Code',
+                result: 'Result'
+            },
+            {
+                key: '3',
+                index: '1',
+                subject: 'Subject',
+                course: 'Course',
+                module: 'Module',
+                code: 'Code',
+                result: 'Result'
+            },
+        ];
+    
+        //search box
+        const { Search } = Input;
+        const onSearch = value => console.log(value);
+
+
+    return(
+        <div>
+            <Search placeholder="Search Result" onSearch={onSearch} enterButton className="searchbar" />
+            <center><h1 className="Heading">My Performance</h1></center>
+            <p className="perLabels1">Student Id:</p><p></p>
+            <p className="perLabels2"> Student Name:</p><p></p>
+            <Table columns={columns} dataSource={data} size="middle" pagination={false} className="crsTable" />
+        </div>
+    );
+}
+
+export default MyPerformance;
