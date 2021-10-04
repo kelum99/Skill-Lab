0d1bc9c6badf28773871a6d2f995c8db867b211c
Leo <lumbinibimsara98@gmail.com>
2021-10-05 01:13:09 +0530

    authentication edited

--- src/pages/Authentication Management/AuthenticationReport.js
+++ src/pages/Authentication Management/AuthenticationReport.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './stylesManagement.css'
-import { Table, Button, Input, Popconfirm, message } from 'antd';
-import { DeleteOutlined, PrinterOutlined, DownloadOutlined } from '@ant-design/icons';
+import { Table, Button, Input, message } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 import { jsPDF } from "jspdf"
 import useRequest from "../../services/RequestContext";
 import moment from "moment";
@@ -15,44 +15,39 @@ function LecManagement() {
     const { request } = useRequest();
     let doc;
 
+        function confirm(e) {
+            console.log(e);
+            message.success('Click on Yes');
+         }
+
+        function cancel(e) {
+            console.log(e);
+            message.error('Click on No');
+        }
 
 
-
-    function confirm(e) {
-        console.log(e);
-        message.success('Click on Yes');
-    }
-
-    function cancel(e) {
-        console.log(e);
-        message.error('Click on No');
-    }
-
-
-    const fetchAuthenticationLecturer = async () => {
+        const fetchAuthenticationLecturer = async () => {
         setLoading(true);
 
         try {
             const result = await request.get("AuthenticationRoute/CommonSignup");
 
             if (result.status === 200) {
-                // setData(result.data);
                 setData(result.data.map(vl => ({ ...vl, birthday: moment(vl.birthday).local().format("YYYY-MM-DD") })));
             }
-            console.log(" lec list get ", result);
-            setLoading(false);
-        } catch (e) {
-            setLoading(false);
-        }
-    };
+                console.log(" lec list get ", result);
+                setLoading(false);
+            } catch (e) {
+                setLoading(false);
+            }
+        };
 
-    useEffect(() => {
+        useEffect(() => {
         fetchAuthenticationLecturer();
-    }, []);
+        }, 
+        []);
 
-
-
-    const onDelete = async value => {
+        const onDelete = async value => {
         try {
 
             const result = await request.delete(`AuthenticationRoute/CommonSignup/${value._id}`);
@@ -103,20 +98,16 @@ function LecManagement() {
             dataIndex: 'number',
             key: 'number',
         },
-
-
         {
             title: <b>Role</b>,
             dataIndex: 'role',
             key: 'role',
         },
 
-
     ];
 
-
-
     //search box
+
     const { Search } = Input;
 
     const onSearch = (value) => {
@@ -138,22 +129,14 @@ function LecManagement() {
 
     const downloadPDF = () => {
 
-
-
         doc = new jsPDF({
-
             orientation: "landscape",
-
             unit: "pt",
-
             format: [2500, 1000]
-
         })
 
         doc.html(document.getElementById("autReport"), {
-
             callback: function (pdf) {
-
                 pdf.save("AuthenticationReport.pdf");
 
             },
@@ -162,24 +145,18 @@ function LecManagement() {
 
     }
 
-
-
-
     return (
         <div className="Au-manage">
             <Search placeholder="Search Here" onSearch={onSearch} enterButton className="searchbar" />
 
-
-           
-
             <div id="autReport">
-
-                
 
                 <div className="aut-report-Head">
                     <center><img src={logo} alt="logo" className="logoAut" /></center>
-                    <h5>SKILL-LAB</h5><p>Tel:- 011-2548741<br />e-mail:- skilllab.edu@gmail.com</p></div>
-                    <br /><br /><h1 className="autcenter"><center>Authentication Report</center></h1>
+                    <h5>SKILL-LAB</h5><p>Tel:- 011-2548741<br />e-mail:- skilllab.edu@gmail.com</p>
+                </div>
+                
+                <br/><br/><h1 className="autcenter"><center>Authentication Report</center></h1>
 
                 <Table columns={columns} dataSource={data} size="middle" pagination={false} className="tbl" />
             </div>
--- src/pages/Authentication Management/LecManagement.js
+++ src/pages/Authentication Management/LecManagement.js
@@ -1,7 +1,7 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './stylesManagement.css'
-import { Table, Button,Input,Popconfirm, message } from 'antd';
-import { DeleteOutlined,PrinterOutlined} from '@ant-design/icons';
+import { Table, Button, Input, Popconfirm, message } from 'antd';
+import { DeleteOutlined, PrinterOutlined } from '@ant-design/icons';
 import useRequest from "../../services/RequestContext";
 import moment from "moment";
 import { Link } from "react-router-dom";
@@ -9,165 +9,158 @@ import { Link } from "react-router-dom";
 
 function LecManagement() {
 
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const {request} = useRequest();
-  
+  const { request } = useRequest();
 
 
 
-function confirm(e) {
-  console.log(e);
-  message.success('Click on Yes');
-}
 
-function cancel(e) {
-  console.log(e);
-  message.error('Click on No');
-}
-
-
-const fetchAuthenticationLecturer = async () => {
-  setLoading(true);
-
-  try {
-    const result = await request.get("AuthenticationRoute/CommonSignup");
-    
-    if (result.status === 200) {
-      // setData(result.data);
-      setData(result.data.map(vl => ({...vl, birthday: moment(vl.birthday).local().format("YYYY-MM-DD")})));
-    }
-    console.log(" lec list get ", result);
-    setLoading(false);
-  } catch (e) {
-    setLoading(false);
+  function confirm(e) {
+    console.log(e);
+    message.success('Click on Yes');
   }
-};
 
-useEffect(() => {
-  fetchAuthenticationLecturer();
-}, []);
-
-
-
-const onDelete = async value =>{
-  try{
-   
-    const result = await request.delete(`AuthenticationRoute/CommonSignup/${value._id}`);
-   
-    console.log("api call data deleted" , result);
-    window.location.reload(true);
-  }catch(e){
-    console.log("error",e);
+  function cancel(e) {
+    console.log(e);
+    message.error('Click on No');
   }
-};
 
-//table
+  const fetchAuthenticationLecturer = async () => {
+    setLoading(true);
 
-const columns = [
-   
-  {
-    title: <b>First Name</b>,
-    dataIndex: 'name',
-    key:'name',
-  },
-  {
-    title: <b>Last Name</b>,
-    dataIndex: 'name1',
-    key:'name1',
-  },
-  {
-    title: <b>Birthday</b>,
-    dataIndex: 'birthday',
-    key:'birthday',
-  },
-  {
-    title: <b>Gender</b>,
-    dataIndex: 'gender',
-    key:'gender',
-  },
-  {
-    title: <b>NIC</b>,
-    dataIndex: 'nic',
-    key:'nic',
-  },
-  {
-    title: <b>Email</b>,
-    dataIndex: 'email',
-    key:'email',
-  },
-  {
-    title: <b>Mobile No.</b>,
-    dataIndex: 'number',
-    key:'number',
-  },
-  
-  
-  {
-    title: <b>Role</b>,
-    dataIndex: 'role',
-    key:'role',
-  },
-  {
-    title: <b>Action</b>,
-    dataIndex: 'action',
-    key:'action',
-    render:(text, record,index)=>(
-      <React.Fragment key={index}>
-          
-          <Popconfirm placement="right"  title="Are you sure to delete this task?"
-                onConfirm={()=>onDelete(record)}
-                onCancel={cancel}
-                okText="Yes"
-                cancelText="No" >
-                 <Button type="primary"icon={<DeleteOutlined />} className="dlt" />
-        </Popconfirm>
+    try {
+      const result = await request.get("AuthenticationRoute/CommonSignup");
 
-      
-        </React.Fragment>
+      if (result.status === 200) {
         
-    )
-  },
-      
-  ];
-  
-  
+        setData(result.data.map(vl => ({ ...vl, birthday: moment(vl.birthday).local().format("YYYY-MM-DD") })));
+      }
+      console.log(" lec list get ", result);
+      setLoading(false);
 
-//search box
+    } catch (e) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAuthenticationLecturer();
+  }, []);
+
+  const onDelete = async value => {
+    try {
+
+      const result = await request.delete(`AuthenticationRoute/CommonSignup/${value._id}`);
+
+      console.log("api call data deleted", result);
+      window.location.reload(true);
+
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
+
+  //table
+
+  const columns = [
+
+    {
+      title: <b>First Name</b>,
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: <b>Last Name</b>,
+      dataIndex: 'name1',
+      key: 'name1',
+    },
+    {
+      title: <b>Birthday</b>,
+      dataIndex: 'birthday',
+      key: 'birthday',
+    },
+    {
+      title: <b>Gender</b>,
+      dataIndex: 'gender',
+      key: 'gender',
+    },
+    {
+      title: <b>NIC</b>,
+      dataIndex: 'nic',
+      key: 'nic',
+    },
+    {
+      title: <b>Email</b>,
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: <b>Mobile No.</b>,
+      dataIndex: 'number',
+      key: 'number',
+    },
+    {
+      title: <b>Role</b>,
+      dataIndex: 'role',
+      key: 'role',
+    },
+    {
+      title: <b>Action</b>,
+      dataIndex: 'action',
+      key: 'action',
+      render: (text, record, index) => (
+        <React.Fragment key={index}>
+
+          <Popconfirm placement="right" title="Are you sure to delete this task?"
+            onConfirm={() => onDelete(record)}
+            onCancel={cancel}
+            okText="Yes"
+            cancelText="No" >
+            <Button type="primary" icon={<DeleteOutlined />} className="dlt" />
+          </Popconfirm>
+
+        </React.Fragment>
+
+      )
+    },
+
+  ];
+
+  //search box
   const { Search } = Input;
 
-    const onSearch =  (value) => {
-        let result = [];
-        result = data.filter((data) =>{
-            
-            if(value == ""){
-                window.location.reload(true);
-                return data;
-                
-            }else{
-            return data.nic.toLowerCase().search(value) != -1 || data.name.toLowerCase().search(value) != -1 
-            || data.name1.toLowerCase().search(value) != -1  || data.email.toLowerCase().search(value) != -1  
-            || data.role.toLowerCase().search(value) != -1       
-            }
-        });
-        setData(result);
-    };
-  
-   
+  const onSearch = (value) => {
+    let result = [];
+    result = data.filter((data) => {
+
+      if (value == "") {
+        window.location.reload(true);
+        return data;
+
+      } else {
+        return data.nic.toLowerCase().search(value) != -1 || data.name.toLowerCase().search(value) != -1
+          || data.name1.toLowerCase().search(value) != -1 || data.email.toLowerCase().search(value) != -1
+          || data.role.toLowerCase().search(value) != -1
+      }
+    });
+    setData(result);
+  };
 
   return (
-        <div className="Au-manage">
-            <Search placeholder="Search Here" onSearch={onSearch} enterButton className="searchbar" />
+    <div className="Au-manage">
+      <Search placeholder="Search Here" onSearch={onSearch} enterButton className="searchbar" />
 
-              <br /><br /><h1 className="Heading1">Authentication Administrator</h1>
-              
-          <Link to ="/authenticationreport"> <Button type="primary" className="btnReport" icon={<PrinterOutlined/>}> Get Report </Button></Link>
+      <br /><br /><h1 className="Heading1">Authentication Administrator</h1>
 
+      <Link to="/authenticationreport">
+         <Button type="primary" className="btnReport" icon={<PrinterOutlined />}> Get Report </Button>
+      </Link>
 
-            <Table columns={columns} dataSource={data} size="middle" pagination={false} className="tbl" />
-         
-        </div>
-    );
+      <Table columns={columns} dataSource={data} size="middle" pagination={false} className="tbl" />
+
+    </div>
+  );
 }
 
 export default LecManagement;
